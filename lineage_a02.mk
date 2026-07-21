73d4f76f71813262744c5d0b17e9d7e4bc2d1d3c
rdbckp <ardibackup@gmail.com>
2026-07-21 18:29:21 +0700

    Update lineage_a02.mk

--- lineage_a02.mk
+++ lineage_a02.mk
@@ -1,3 +1,9 @@
+# Inherit Lineage common config (WAJIB - ini yang isi LINEAGE_VERSION,
+# LINEAGE_BUILD, dan seluruh identitas "Lineage" di product config.
+# Tanpa baris ini, produk cuma AOSP polos yang dikasih nama lineage_a02,
+# dan nama file zip jadi kosong: lineage-.zip)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from device tree
